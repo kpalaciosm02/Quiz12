a6da4fde5f6d1f24858c9b9aee3ec2db63811c80
kpalacios <kpalaciosm02@gmail.com>
2021-10-09 19:37:47 -0600

    Truck class created but not 100% ready.

---
+++ Truck.hpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+#ifndef TRUCK
+#define TRUCk
+class Truck{
+    private:
+        int speed;
+        string direction;
+        bool started;
+        int tankPercentage;
+        int tankSize;
+        int consumption;
+    public:
+
+};
+
+#endif
